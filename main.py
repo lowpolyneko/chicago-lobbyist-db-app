5ffd136fb742455936089da56a0cd98098c8f423
Neko <lowpolyneko@protonmail.ch>
2024-10-07 21:14:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#
+# header comment!
+#
+import sqlite3
+import objecttier
+
+##################################################################  
+#
+# main
+#
+print('** Welcome to the Chicago Lobbyist Database Application **')
+
+print("Please enter a command (1-5, x to exit): ")
+print("**Error, unknown command, try again...")
+
+#
+# done
+#
---
+++ objecttier.py
@@ -0,0 +1,182 @@
+#
+# objecttier
+#
+# Builds Lobbyist-related objects from data retrieved through 
+# the data tier.
+#
+# Original author: Ellen Kidane
+#
+import datatier
+
+
+##################################################################
+#
+# Lobbyist:
+#
+# Constructor(...)
+# Properties:
+#   Lobbyist_ID: int
+#   First_Name: string
+#   Last_Name: string
+#   Phone: string
+#
+class Lobbyist:
+   pass
+
+##################################################################
+#
+# LobbyistDetails:
+#
+# Constructor(...)
+# Properties:
+#   Lobbyist_ID: int
+#   Salutation: string
+#   First_Name: string
+#   Middle_Initial: string
+#   Last_Name: string
+#   Suffix: string
+#   Address_1: string
+#   Address_2: string
+#   City: string
+#   State_Initial: string
+#   Zip_Code: string
+#   Country: string
+#   Email: string
+#   Phone: string
+#   Fax: string
+#   Years_Registered: list of years
+#   Employers: list of employer names
+#   Total_Compensation: float
+#
+class LobbyistDetails:
+   pass
+
+##################################################################
+#
+# LobbyistClients:
+#
+# Constructor(...)
+# Properties:
+#   Lobbyist_ID: int
+#   First_Name: string
+#   Last_Name: string
+#   Phone: string
+#   Total_Compensation: float
+#   Clients: list of clients
+#
+class LobbyistClients:
+   pass
+
+##################################################################
+# 
+# num_lobbyists:
+#
+# Returns: number of lobbyists in the database
+#           If an error occurs, the function returns -1
+#
+def num_lobbyists(dbConn):
+   pass
+
+##################################################################
+# 
+# num_employers:
+#
+# Returns: number of employers in the database
+#           If an error occurs, the function returns -1
+#
+def num_employers(dbConn):
+   pass
+
+##################################################################
+# 
+# num_clients:
+#
+# Returns: number of clients in the database
+#           If an error occurs, the function returns -1
+#
+def num_clients(dbConn):
+   pass
+
+##################################################################
+#
+# get_lobbyists:
+#
+# gets and returns all lobbyists whose first or last name are "like"
+# the pattern. Patterns are based on SQL, which allow the _ and % 
+# wildcards.
+#
+# Returns: list of lobbyists in ascending order by ID; 
+#          an empty list means the query did not retrieve
+#          any data (or an internal error occurred, in
+#          which case an error msg is already output).
+#
+def get_lobbyists(dbConn, pattern):
+   pass
+
+
+##################################################################
+#
+# get_lobbyist_details:
+#
+# gets and returns details about the given lobbyist
+# the lobbyist id is passed as a parameter
+#
+# Returns: if the search was successful, a LobbyistDetails object
+#          is returned. If the search did not find a matching
+#          lobbyist, None is returned; note that None is also 
+#          returned if an internal error occurred (in which
+#          case an error msg is already output).
+#
+def get_lobbyist_details(dbConn, lobbyist_id):
+   pass
+         
+
+##################################################################
+#
+# get_top_N_lobbyists:
+#
+# gets and returns the top N lobbyists based on their total 
+# compensation, given a particular year
+#
+# Returns: returns a list of 0 or more LobbyistClients objects;
+#          the list could be empty if the year is invalid. 
+#          An empty list is also returned if an internal error 
+#          occurs (in which case an error msg is already output).
+#
+def get_top_N_lobbyists(dbConn, N, year):
+   pass
+
+
+##################################################################
+#
+# add_lobbyist_year:
+#
+# Inserts the given year into the database for the given lobbyist.
+# It is considered an error if the lobbyist does not exist (see below), 
+# and the year is not inserted.
+#
+# Returns: 1 if the year was successfully added,
+#          0 if not (e.g. if the lobbyist does not exist, or if
+#          an internal error occurred).
+#
+def add_lobbyist_year(dbConn, lobbyist_id, year):
+   pass
+
+
+##################################################################
+#
+# set_salutation:
+#
+# Sets the salutation for the given lobbyist.
+# If the lobbyist already has a salutation, it will be replaced by
+# this new value. Passing a salutation of "" effectively 
+# deletes the existing salutation. It is considered an error
+# if the lobbyist does not exist (see below), and the salutation
+# is not set.
+#
+# Returns: 1 if the salutation was successfully set,
+#          0 if not (e.g. if the lobbyist does not exist, or if
+#          an internal error occurred).
+#
+def set_salutation(dbConn, lobbyist_id, salutation):
+   pass
