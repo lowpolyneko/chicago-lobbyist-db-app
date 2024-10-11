eb73c6571b8c4ec314d2c5981e958fe812229934
Neko <lowpolyneko@protonmail.ch>
2024-10-11 13:34:48 -0500

    lower via python

--- objecttier.py
+++ objecttier.py
@@ -287,7 +287,7 @@ def get_lobbyists(dbConn, pattern):
     # run query
     res = datatier.select_n_rows(dbConn, f"""
     SELECT Lobbyist_ID, First_Name, Last_Name, Phone FROM LobbyistInfo
-    WHERE LOWER(First_Name) = LOWER('{pattern}') OR LOWER(Last_Name) = LOWER('{pattern}')
+    WHERE LOWER(First_Name) = '{pattern.lower()}' OR LOWER(Last_Name) = '{pattern.lower()}'
     ORDER BY Lobbyist_ID ASC
     """)
 
