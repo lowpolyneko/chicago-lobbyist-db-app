8eef2df6bebb9ba934ee8d4a672a9c9a8ae16436
Neko <lowpolyneko@protonmail.ch>
2024-10-14 19:43:30 -0500

    finished i think

--- main.py
+++ main.py
@@ -11,14 +11,22 @@
 import sqlite3
 import objecttier
 
+
 ##################################################################  
 #
 # command 1
 #
 def command_1(db: sqlite3.Connection):
+    """
+    Given the user input of a name, find and output basic information for all
+    lobbyists that match that name.
+
+    @param db database
+    """
     search = input('\nEnter lobbyist name (first or last, wildcards _ and % supported): ')
     lobbyists = objecttier.get_lobbyists(db, search)
 
+    # check for too many items
     if len(lobbyists) > 100:
         print('\nThere are too many lobbyists to display, please narrow your search and try again...')
         return
@@ -34,9 +42,16 @@ def command_1(db: sqlite3.Connection):
 # command 2
 #
 def command_2(db: sqlite3.Connection):
+    """
+    Given a lobbyist ID, find and output detailed information about the
+    lobbyist.
+
+    @param db database
+    """
     search = input('\nEnter Lobbyist ID: ')
     details = objecttier.get_lobbyist_details(db, search)
 
+    # error check
     if not details:
         print('\nNo lobbyist with that ID was found.')
         return
@@ -49,11 +64,97 @@ def command_2(db: sqlite3.Connection):
     print(f'  Fax: {details.Fax}')
     print('  Years Registered: ', end='')
     for y in details.Years_Registered:
-        print(f'{y}', end=', ')
+        print(y, end=', ')
     print('\n  Employers: ', end='')
     for e in details.Employers:
-        print(f'{e}', end=', ')
+        print(e, end=', ')
     print(f'\n  Total Compensation: ${details.Total_Compensation:.02f}')
+
+
+##################################################################  
+#
+# command 3
+#
+def command_3(db: sqlite3.Connection):
+    """
+    Given a year, output the top N lobbyists based on their total compensation
+    for that year.
+
+    @param db database
+    """
+    N = input('\nEnter the value of N: ')
+    if int(N) < 1:
+        print('Please enter a positive value for N...')
+        return
+
+    year = input('Enter the year: ')
+
+    top_n_lobbyists = objecttier.get_top_N_lobbyists(db, N, year)
+
+    # enumerate for index
+    for i, l in enumerate(top_n_lobbyists):
+        print(f'{i} . {l.First_Name} {l.Last_Name}')
+        print(f'  Phone: {l.Phone}')
+        print(f'  Total Compensation: ${l.Total_Compensation:.02f}')
+        print('  Clients: ', end='')
+        for c in l.Clients:
+            print(c, end=', ')
+
+
+##################################################################  
+#
+# command 4
+#
+def command_4(db: sqlite3.Connection):
+    """
+    Register an existing lobbyist for a new year.
+
+    @param db database
+    """
+    year = input('\nEnter year: ')
+    search = input('Enter the lobbyist ID: ')
+
+    # 1 for success, 0 for failure
+    if objecttier.add_lobbyist_year(db, search, year):
+        print('\nLobbyist successfully registered.')
+    else:
+        print('\nNo lobbyist with that ID was found.')
+
+
+##################################################################  
+#
+# command 5
+#
+def command_5(db: sqlite3.Connection):
+    """
+    Set the salutation for a given lobbyist.
+
+    @param db database
+    """
+    search = input('\nEnter the lobbyist ID: ')
+    salutation = input('Enter the salutation: ')
+
+    # 1 for success, 0 for failure
+    if objecttier.set_salutation(db, search, salutation):
+        print('\nSalutation successfully set.')
+    else:
+        print('\nNo lobbyist with that ID was found.')
+
+
+##################################################################  
+#
+# print stats
+#
+def print_stats(db: sqlite3.Connection):
+    """
+    Print general statistics of the database.
+
+    @param db database
+    """
+    print('\nGeneral Statistics:')
+    print(f'  Number of Lobbyists: {objecttier.num_lobbyists(db):,}')
+    print(f'  Number of Employers: {objecttier.num_employers(db):,}')
+    print(f'  Number of Clients: {objecttier.num_clients(db):,}')
 
 
 ##################################################################  
@@ -61,11 +162,13 @@ def command_2(db: sqlite3.Connection):
 # main
 #
 def main():
-    print('** Welcome to the Chicago Lobbyist Database Application **')
-
+    # open db connection
     db = sqlite3.connect('Chicago_Lobbyists.db')
 
+    print('** Welcome to the Chicago Lobbyist Database Application **')
+    print_stats(db)
 
+    # main eval loop
     while True:
         i = input('\nPlease enter a command (1-5, x to exit): ')
 
@@ -76,13 +179,13 @@ def main():
             command_2(db)
             continue
         elif i == '3':
-            # command_3(db)
+            command_3(db)
             continue
         elif i == '4':
-            # command_4(db)
+            command_4(db)
             continue
         elif i == '5':
-            # command_5(db)
+            command_5(db)
             continue
         elif i == 'x':
             break
